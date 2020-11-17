278f787033e30b2b9307cc3b8f296e3829e6752a
William Wang <zeweiwang@outlook.com>
2020-11-18 00:39:57 +0800

    Parser: use new parser to parse http head

--- src/main.rs
+++ src/main.rs
@@ -36,37 +36,43 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "error/404.html")
-    };
-
+            
     println!("Raw request:\n{}", String::from_utf8_lossy(&buffer[..]));
     let buf_str = &String::from_utf8_lossy(&buffer[..]);
     // let request = parse_http_head(buf_str).unwrap();
     let request = HttpRequest::from(buf_str as &str);
     println!("Parsed request: {}", request);
     
-    if request.method == HttpRequestMethod::ILLEGAL {
-        println!("Ignored illegal request");
-    }
-
-    let page_dir = "/mnt/c/Workpath/rhttp/page/";
-    let full_filename = format!("{}{}", page_dir, filename);
-    println!("Return file {}\n", full_filename);
-
-    let contents = fs::read_to_string(full_filename).unwrap();
-
+    let page_dir = "/mnt/c/Workpath/rhttp/page"; // TODO: add to config
+    
+    let (status_line, filename) = match request.method {
+        HttpRequestMethod::ILLEGAL => {
+            println!("Ignored illegal request");
+            return;
+        }
+        HttpRequestMethod::GET => {
+            // check if requsested resource exists
+            if request.url == "/" {
+                ("HTTP/1.1 200 OK\r\n\r\n", format!("{}/index.html", page_dir))
+            } else {
+                let full_filename = format!("{}{}", page_dir, request.url);
+                match fs::File::open(&full_filename) {
+                    Ok(_) => ("HTTP/1.1 200 OK\r\n\r\n", full_filename), // if resource exists, return it to client
+                    _ => ("HTTP/1.1 404 NOT FOUND\r\n\r\n", format!("{}/error/404.html", page_dir)), // otherwise, 404
+                }
+            }
+        }
+        HttpRequestMethod::POST => {
+            println!("POST is not supported for now");
+            return;
+        }
+    };
+    
+    println!("Return file {}\n", filename);
+    
+    let contents = fs::read_to_string(filename).unwrap();
     let response = format!("{}{}", status_line, contents);
-
+    
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
